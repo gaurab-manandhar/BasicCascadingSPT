d7b66242f70dc1e29639c24319211e571f97786f
Gaurab Manandhar <gaurabmdr@gmail.com>
2018-10-13 21:08:46 +0545

    Question2_Demerge_Gender

--- src/main/java/cascading_assignment/assignment/question2_demerge_pipe/DeMerge_Pipe_Main.java
+++ src/main/java/cascading_assignment/assignment/question2_demerge_pipe/DeMerge_Pipe_Main.java
@@ -19,16 +19,25 @@ public class DeMerge_Pipe_Main {
 
     public static void main(String[] args) {
 
+        /**
+         * Resource Paths (source, Male , Female and Unknown)
+         * */
         String sourcePath = "src/main/resources/cascading_assignment/question2_demerge_pipe/cascading_demerge_gender_input.txt";
         String sinkMPath = "src/main/resources/cascading_assignment/question2_demerge_pipe/cascading_demerge_male_expected.txt";
         String sinkFPath = "src/main/resources/cascading_assignment/question2_demerge_pipe/cascading_demerge_female_expected.txt";
         String sinkUPath = "src/main/resources/cascading_assignment/question2_demerge_pipe/cascading_demerge_unknown_group_expected.txt";
 
+        /**
+         * Tap for defining the input and output delimeter
+         * */
         Tap sourceTap = new FileTap(new TextDelimited(true, ";"), sourcePath);
         Tap sinkMTap = new FileTap(new TextDelimited(true, "|"), sinkMPath, SinkMode.REPLACE);
         Tap sinkFTap = new FileTap(new TextDelimited(true, "|"), sinkFPath, SinkMode.REPLACE);
         Tap sinkUTap = new FileTap(new TextDelimited(true, "|"), sinkUPath, SinkMode.REPLACE);
 
+        /**
+         * Main pipe to demerge pipe for gender seperation
+         * */
         Pipe genderPipe = new Pipe("genderPipe");
 
         Pipe mPipe = new Pipe("malePipe", genderPipe);
@@ -38,6 +47,10 @@ public class DeMerge_Pipe_Main {
         mPipe = new Each(mPipe, new Fields("gender"), new GenderFilter("M"));
         fPipe = new Each(fPipe, new Fields("gender"), new GenderFilter("F"));
         uPipe = new Each(uPipe, new Fields("gender"), new GenderFilter("U"));
+
+        /**
+         * Adding filter output to the  diferrent sink pipe
+         * */
 
         FlowDef flowDef = FlowDef
                 .flowDef()
